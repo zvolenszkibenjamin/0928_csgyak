9c10c3b491807468dca3767a12226b1312506d7d
Zvolenszki Benjámin <zvolenszki.benjamin@vereb.dc>
2020-09-28 08:44:48 +0200

    Add project files.

---
+++ 0928_csgyak/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0928_csgyak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            string txt = "GitHub próba de nem tudom minek,";
+            string txt2 = "főleg hogy én már használom de mindegy";
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (txt.Length / 2), (Console.WindowHeight / 2) - 1);
+            Console.Write(txt);
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (txt2.Length / 2), Console.WindowHeight / 2);
+            Console.Write(txt2);
+            Console.ReadKey(true);
+        }
+    }
+}
